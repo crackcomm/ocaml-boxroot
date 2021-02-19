6dbf005073444d466bef5304fd7adb73c12ac5b4
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2021-02-19 19:39:39 +0100

    Make C calls for gc @noalloc

--- benchmarks/lib-ref/gc_ref.ml
+++ benchmarks/lib-ref/gc_ref.ml
@@ -1,5 +1,5 @@
 type 'a t
 external create : 'a -> 'a t         = "gc_ref_create"
-external get : 'a t -> 'a            = "gc_ref_get"
-external modify : 'a t -> 'a -> unit = "gc_ref_modify"
-external delete : 'a t -> unit       = "gc_ref_delete"
+external get : 'a t -> 'a            = "gc_ref_get" [@@noalloc]
+external modify : 'a t -> 'a -> unit = "gc_ref_modify" [@@noalloc]
+external delete : 'a t -> unit       = "gc_ref_delete" [@@noalloc]
