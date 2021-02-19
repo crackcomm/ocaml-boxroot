5257bbd1fd3be6dbe42140c1017a3b046106f9d0
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@Inria.fr>
2021-02-19 21:19:15 +0100

    Add globroots to README, improve it

--- benchmarks/lib-ref/boxroot_ref.ml
+++ benchmarks/lib-ref/boxroot_ref.ml
@@ -1,8 +1,8 @@
 type 'a t
 external create : 'a -> 'a t         = "boxroot_ref_create"
-external get : 'a t -> 'a            = "boxroot_ref_get"
-external modify : 'a t -> 'a -> unit = "boxroot_ref_modify"
-external delete : 'a t -> unit       = "boxroot_ref_delete"
+external get : 'a t -> 'a            = "boxroot_ref_get" [@@noalloc]
+external modify : 'a t -> 'a -> unit = "boxroot_ref_modify" [@@noalloc]
+external delete : 'a t -> unit       = "boxroot_ref_delete" [@@noalloc]
 
 external setup : unit -> unit = "boxroot_ref_setup"
 external teardown : unit -> unit = "boxroot_ref_teardown"
