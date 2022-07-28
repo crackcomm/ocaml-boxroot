2cbfc61c2ef254054e3cdab3d8404ad656cd636a
Guillaume Munch-Maccagnoni <Guillaume.Munch-Maccagnoni@inria.fr>
2022-07-28 02:13:44 +0200

    Show stats again in gc and ocaml

--- benchmarks/lib-ref/gc_ref.ml
+++ benchmarks/lib-ref/gc_ref.ml
@@ -4,6 +4,6 @@ external get : 'a t -> 'a            = "gc_ref_get" [@@noalloc]
 external modify : 'a t array -> int -> 'a -> unit = "gc_ref_modify" [@@noalloc]
 external delete : 'a t -> unit       = "gc_ref_delete" [@@noalloc]
 
-let setup () = ()
-let teardown () = ()
-let print_stats () = ()
+external setup : unit -> unit = "boxroot_ref_setup"
+external teardown : unit -> unit = "boxroot_ref_teardown"
+external print_stats : unit -> unit = "boxroot_stats"
